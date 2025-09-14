1b9b5bd0e984b8ed68b9d5533955b1f8f4dde20a
turqayisgndrov0-web <turqayisgndrov0@gmail.com>
2025-09-14 11:56:56 +0100

    java script

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let eded1 = Number(prompt("Birinci ededi secin"));
+let eded2 = Number(prompt("Ikinci ededi secin"));
+let cem = eded1 + eded2;
+alert("Cem: " + cem)
+for (let i = 1; i <= 10 ; i++) 
+console.log(i);
+function kvadrat(x){
+    return x * x;
+}
+console.log("5-in kvadrati: " + kvadrat(5));
